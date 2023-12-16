b46049a6d43c4fc628b79ee8b8acf45f0e7cc557
Taufiq Ahmed <taufiqahmed181@gmail.com>
2023-12-16 23:02:23 +0530

    Asiignment

---
+++ pages/orders.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const orders = () => {
+  return (
+    
+<div>
+  <h1 className='text-4xl h-20 py-6 px-6 font-bold border-2'>Orders</h1>
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg my-2">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Order-Id
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Products
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    262727
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    Delivered
+                </td>
+                <td class="px-6 py-4">
+                    ₹2999
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+
+  )
+}
+
+export default orders
